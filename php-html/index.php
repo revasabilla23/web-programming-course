a29c95c5a47d8a0ecf9ba4890ae71de6029a9412
Reva Putria Sabilla <if23.revasabilla@mhs.ubpkarawang.ac.id>
2024-11-21 10:45:43 +0700

    index.php

--- php-html/index.php
+++ php-html/index.php
@@ -3,60 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman Login</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-            background-color: #f0f0f0;
-        }
-
-        .login-container {
-            text-align: center;
-            padding: 40px;
-            width: 300px;
-            background-color: white;
-            border-radius: 10px;
-            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-        }
-
-
-        input[type="email"], input[type="password"] {
-            width: 100%;
-            padding: 10px;
-            margin: 10px 0;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-        }
-
-        button {
-            padding: 10px 20px;
-            background-color: lightcoral;
-            color: white;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-
-        button:hover {
-            background-color: lightpink;
-        }
-    </style>
+    <title>Document</title>
 </head>
 <body>
-
-    <div class="login-container">
-        <h1>Halaman Login</h1>
-        <form action="./backend/login.php" method="POST">
-            <input type="email" name="email" id="email" placeholder="Email" required />
-            <input type="password" name="password" id="password" placeholder="Password" required />
-            <button type="submit">LOGIN</button>
-        </form>
-    </div>
+    <h1>login</h1>
+    <form action="./backend/login.php" method="post">
+        <input type="email" name="email" placeholder="masukkan email anda">
+        <input type="password" name="password" placeholder="masukkan password anda">
+        <input type="submit" value="login" name="submit">
+    </form>
 
 </body>
 </html>
